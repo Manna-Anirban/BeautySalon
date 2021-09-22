396f12ba30605d8e5a3f5710195e84f6d87d9a53
Manna-Anirban <anirbanm@skechers.com>
2021-09-22 03:43:17 -0700

    Image update

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ sideNav.style.right = "-250px"
 menuBtn.onclick = function(){
     if(sideNav.style.right == "-250px"){
         sideNav.style.right = "0"
-        menu.src = "img/close.PNG"
+        menu.src = "img/close.png"
     }
     else{
         sideNav.style.right = "-250px"
-        menu.src = "img/menu.PNG"
+        menu.src = "img/menu.png"
     }
 }
 
